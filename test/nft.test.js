973755eb07589e3baf02967671a3c0ffac249813
chloet.klaytn <chloet@chloetklaytns-MacBook-Pro.local>
2022-01-27 23:07:56 +0800

    first commit

---
+++ test/nft.test.js
@@ -0,0 +1,33 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("NFT", () => {
+  beforeEach(async () => {
+    [owner, minter, receiver] = await ethers.getSigners();
+    const NFT = await ethers.getContractFactory("NFT");
+    nft = await NFT.deploy();
+    await nft.deployed();
+  });
+
+  describe("Deployment", async () => {
+    it("has a name", async () => {
+      expect(await nft.name()).to.equal("Gallery Token");
+    });
+
+    it("has a symbol", async () => {
+      expect(await nft.symbol()).to.equal("GTKN");
+    });
+
+    it("sets the owner as contract deployer", async () => {
+      expect(await nft.owner()).to.equal(owner.address);
+    });
+  });
+
+  describe("Minting", async () => {
+    it("mints tokens to msg sender", async () => {});
+  });
+
+  describe("Transfers", async () => {
+    it("transfers tokens", async () => {});
+  });
+});
