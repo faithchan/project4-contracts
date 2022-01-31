9c6ea6374c17f5d17bea1c9bfe7a656864460c7e
chloet <chloet@chloetklaytns-MacBook-Pro.local>
2022-01-31 10:05:30 +0800

    successfully tested marketplace deployment

--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -4,12 +4,12 @@ const { ethers } = require('hardhat')
 describe('Marketplace', () => {
   let marketplace
   let nft
-  let marketplaceRoyalty = 500
+  let marketplaceFee = 250
 
   beforeEach(async () => {
     ;[contractOwner, minter, receiver] = await ethers.getSigners()
     const Marketplace = await ethers.getContractFactory('Marketplace')
-    marketplace = await Marketplace.deploy(marketplaceRoyalty)
+    marketplace = await Marketplace.deploy(marketplaceFee)
     await marketplace.deployed()
 
     const NFT = await ethers.getContractFactory('NFT')
@@ -17,9 +17,12 @@ describe('Marketplace', () => {
     await nft.deployed()
   })
 
-  // describe('Deployment', async () => {
-  //   it('sets the owner as contract deployer', async () => {
-  //     expect(await marketplace.owner()).to.equal(contractOwner.address)
-  //   })
-  // })
+  describe('Deployment', async () => {
+    it('sets the owner as contract deployer', async () => {
+      expect(await marketplace.owner()).to.equal(contractOwner.address)
+    })
+    it('sets the marketplace fee', async () => {
+      expect(await marketplace.marketplaceFee()).to.equal(marketplaceFee)
+    })
+  })
 })
