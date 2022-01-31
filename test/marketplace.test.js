e699b6726f556dea5ab74c20310522f68032cc1c
chloet <chloet@chloetklaytns-MacBook-Pro.local>
2022-01-31 11:27:45 +0800

    changed js integers to BN to avoid integer overflow/underflow errors

--- test/marketplace.test.js
+++ test/marketplace.test.js
@@ -5,9 +5,9 @@ const { ethers } = require('hardhat')
 describe('Marketplace', () => {
   let marketplace
   let nft
-  let marketplaceFee = 250
-  let royaltyAmount = 600
-  let salePrice = 10
+  let marketplaceFee = ethers.BigNumber.from(250)
+  let royaltyAmount = ethers.BigNumber.from(500)
+  let salePrice = ethers.BigNumber.from(ethers.utils.parseEther('10'))
   const token1URI = 'https://ipfs.io/ipfs/QmXmNSH2dyp5R6dkW5MVhNc7xqV9v3NHWxNXJfCL6CcYxS'
   const token2URI = 'https://ipfs.io/ipfs/QmQ35DkX8HHjhkJe5MsMAd4X51iP3MHV5d5dZoee32J83k'
 
@@ -36,7 +36,9 @@ describe('Marketplace', () => {
     let itemId
     beforeEach(async () => {
       await nft.addToWhitelist(seller.address)
-      const token = await nft.connect(seller).mint(seller.address, token1URI, seller.address, royaltyAmount)
+      const token = await nft
+        .connect(seller)
+        .mint(seller.address, token1URI, seller.address, royaltyAmount)
       const txn = await token.wait()
       tokenId = txn.events[0].args.tokenId
     })
@@ -50,20 +52,37 @@ describe('Marketplace', () => {
     })
 
     it('reverts if non-owner attempts to list item', async () => {
-      await expectRevert(marketplace.listItem(nft.address, tokenId, salePrice), 'Caller does not own token')
+      await expectRevert(
+        marketplace.listItem(nft.address, tokenId, salePrice),
+        'Caller does not own token'
+      )
     })
   })
 
-  // describe('purchaseItem', async () => {
-  //   beforeEach(async () => {
-  //     await nft.addToWhitelist(seller.address)
-  //     const token = await nft.connect(seller).mint(seller.address, token1URI, seller.address, royaltyAmount)
-  //     const txn = await token.wait()
-  //     tokenId = txn.events[0].args.tokenId
-  //   })
-  //   it('transfers token to buyer', async () => {})
-  //   it('transfers marketplace fee to contract owner', async () => {})
-  //   it('transfers royalties to the creator of the token', async () => {})
-  //   it('reverts if ether sent does not equal to the salePrice', async () => {})
-  // })
+  describe('purchaseItem', async () => {
+    let tokenId
+    let itemId
+    beforeEach(async () => {
+      await nft.addToWhitelist(seller.address)
+      const token = await nft
+        .connect(seller)
+        .mint(seller.address, token1URI, seller.address, royaltyAmount)
+      let txn = await token.wait()
+      tokenId = txn.events[0].args.tokenId
+
+      txn = await marketplace.connect(seller).listItem(nft.address, tokenId, salePrice)
+      const receipt = await txn.wait()
+      itemId = receipt.events[0].args.itemId
+
+      await nft.connect(seller).setApprovalForAll(marketplace.address, true)
+    })
+
+    it('transfers token to buyer', async () => {
+      await marketplace.connect(buyer).purchaseItem(nft.address, itemId, { value: salePrice })
+    })
+
+    it('transfers marketplace fee to contract owner', async () => {})
+    it('transfers royalties to the creator of the token', async () => {})
+    it('reverts if ether sent does not equal to the salePrice', async () => {})
+  })
 })
